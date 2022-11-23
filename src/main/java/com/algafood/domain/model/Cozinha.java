97f1489e4b6897b5d515d3cdaad3258b189561ac
Bruno Barros <contato4code@gmail.com>
2022-11-23 07:36:31 -0300

    Duas formas de definir classes de modelo como entidades no banco, com e sem Table name e Column.

---
+++ src/main/java/com/algafood/domain/model/Cozinha.java
@@ -0,0 +1,47 @@
+package com.algafood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_cozinhas")
+public class Cozinha {
+    @Id
+    private Long id;
+
+    @Column(name = "nome_cozinha")
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cozinha cozinha = (Cozinha) o;
+
+        return getId() != null ? getId().equals(cozinha.getId()) : cozinha.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
+    }
+}
---
+++ src/main/java/com/algafood/domain/model/Restaurante.java
@@ -0,0 +1,55 @@
+package com.algafood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Restaurante {
+    @Id
+    private Long id;
+    private String nome;
+    @Column(name = "taxa_frete")
+    private BigDecimal taxaFrete;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Restaurante that = (Restaurante) o;
+
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getTaxaFrete() {
+        return taxaFrete;
+    }
+
+    public void setTaxaFrete(BigDecimal taxaFrete) {
+        this.taxaFrete = taxaFrete;
+    }
+}
