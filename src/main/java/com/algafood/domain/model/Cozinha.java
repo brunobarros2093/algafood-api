b67a7bb12fdfb34389cbefdc748288ece1e7f5c1
Bruno Barros <contato4code@gmail.com>
2022-11-24 19:50:10 -0300

    Adicionado import.sql file. Removido rename da tabela cozinha

--- src/main/java/com/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algafood/domain/model/Cozinha.java
@@ -3,13 +3,11 @@ package com.algafood.domain.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "tb_cozinhas")
 public class Cozinha {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nome_cozinha")
     private String nome;
 
     public Long getId() {
