1e0e876c562b0f7f09a3f0598aaa79ede47525e3
Bruno Barros <contato4code@gmail.com>
2022-11-28 18:16:06 -0300

    Busca Por ID

---
+++ src/main/java/com/algafood/jpa/BuscaCozinhaMain.java
@@ -0,0 +1,23 @@
+package com.algafood.jpa;
+
+import com.algafood.AlgafoodApiApplication;
+import com.algafood.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class BuscaCozinhaMain {
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
+        System.out.println(cozinha);
+
+    }
+}
--- src/main/java/com/algafood/jpa/CadastroCozinha.java
+++ src/main/java/com/algafood/jpa/CadastroCozinha.java
@@ -20,6 +20,10 @@ public class CadastroCozinha {
         return query.getResultList();
     }
 
+    public Cozinha buscar(Long id) {
+        return entityManager.find(Cozinha.class, id);
+    }
+
     @Transactional
     public Cozinha adicionar(Cozinha cozinha) {
         return entityManager.merge(cozinha);
