c8c9752300007c6d99fce92b1607fcc4a204f758
Bruno Barros <contato4code@gmail.com>
2022-12-01 08:09:23 -0300

    Added lombok

--- src/main/java/com/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algafood/domain/model/Cozinha.java
@@ -1,30 +1,19 @@
 package com.algafood.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 public class Cozinha {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
     @Override
     public boolean equals(Object o) {
