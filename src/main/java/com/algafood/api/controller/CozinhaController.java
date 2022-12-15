b9c7b7b1bfdcd83220d0d619b5a5ada629ad6755
brunog.barros <brunog.barros@fornecedores.sicoob.com.br>
2022-12-15 15:06:20 -0300

    Adicionado retorno em xml, e produces no listar.

--- src/main/java/com/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import com.algafood.domain.model.Cozinha;
 import com.algafood.domain.repository.CozinhaRepository;
 import com.algafood.infrastructure.repository.CozinhaRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepositoryImpl cozinhaRepository;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
     }
