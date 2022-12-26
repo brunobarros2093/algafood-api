250459f228cdbfeda148d44e9b0c8b2b18960ca2
brunog.barros <brunog.barros@fornecedores.sicoob.com.br>
2022-12-26 15:23:32 -0300

    Get uma entidade

--- src/main/java/com/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algafood/api/controller/CozinhaController.java
@@ -7,6 +7,7 @@ import com.algafood.infrastructure.repository.CozinhaRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,8 @@ public class CozinhaController {
         return cozinhaRepository.listar();
     }
 
+    @GetMapping("/{id}")
+    public Cozinha buscar(@PathVariable Long id) {
+        return cozinhaRepository.buscar(id);
+    }
 }
