419d111148bf2391a658eb9b60f6c78562a5537e
Bruno Barros <brunog.barros@pm.me>
2022-12-30 15:03:43 -0300

    POST

--- src/main/java/com/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algafood/api/controller/CozinhaController.java
@@ -5,11 +5,9 @@ import com.algafood.domain.model.Cozinha;
 import com.algafood.domain.repository.CozinhaRepository;
 import com.algafood.infrastructure.repository.CozinhaRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +26,11 @@ public class CozinhaController {
     @GetMapping("/{id}")
     public Cozinha buscar(@PathVariable Long id) {
         return cozinhaRepository.buscar(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void adicionar(@RequestBody Cozinha cozinha) {
+        cozinhaRepository.salvar(cozinha);
     }
 }
