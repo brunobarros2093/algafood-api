2e0dad8e9ffcfbdcf81d51a9872d8d4d3a19e883
Bruno Barros <brunog.barros@pm.me>
2023-01-08 22:06:20 -0300

    CadastroCozinhaService

---
+++ src/main/java/com/algafood/domain/service/CadastroCozinhaService.java
@@ -0,0 +1,17 @@
+package com.algafood.domain.service;
+
+import com.algafood.domain.model.Cozinha;
+import com.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroCozinhaService {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+    public Cozinha salvar(Cozinha cozinha) {
+        return cozinhaRepository.salvar(cozinha);
+    }
+
+}
