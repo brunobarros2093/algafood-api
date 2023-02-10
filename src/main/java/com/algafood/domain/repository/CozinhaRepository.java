5d41300ece4283e15c2dc82316dcb090e4c8dd20
brunog.barros <brunog.barros@pm.me>
2023-02-10 16:36:05 -0300

    Filtro query methods

--- src/main/java/com/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algafood/domain/repository/CozinhaRepository.java
@@ -4,10 +4,12 @@ import com.algafood.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-//    List<Cozinha> consultarPorNome(String nome);
+    List<Cozinha> findCozinhaByNome(String nome);
 
 }
