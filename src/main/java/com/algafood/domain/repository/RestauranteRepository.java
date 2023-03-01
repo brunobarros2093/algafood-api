c38fa2fc73f29464caffc5954dbd2bf32a017f9b
brunog.barros <brunog.barros@pm.me>
2023-03-01 14:15:03 -0300

    Adicionada query jpql

--- src/main/java/com/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algafood/domain/repository/RestauranteRepository.java
@@ -2,6 +2,8 @@ package com.algafood.domain.repository;
 
 import com.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -15,6 +17,9 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
     List<Restaurante> findRestauranteByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
     List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
+
+    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
 
     // vai lançar uma exception caso retorna mais de um
     Optional<Restaurante> findFirstByNomeContaining(String nome);
