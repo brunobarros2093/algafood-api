3d55bc229c06f63edf67796af9a04293d8f1ce7d
Bruno B <brunog.barros@pm.me>
2023-05-22 22:45:39 -0300

    Criado Endereco e adicionado como embeddded field no Restaurante

---
+++ src/main/java/com/algafood/domain/model/Endereco.java
@@ -0,0 +1,24 @@
+package com.algafood.domain.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Data
+@Embeddable // incorporável
+public class Endereco {
+
+    @Column(name = "endereco_cep")
+    private String cep;
+    @Column(name = "endereco_logradouro")
+    private String logradouro;
+    @Column(name = "endereco_numero")
+    private String numero;
+    @Column(name = "endereco_complemento")
+    private String complemento;
+    @Column(name = "endereco_bairro")
+    private String bairro;
+    @Column(name = "endereco_cidade")
+    private String cidade;
+}
--- src/main/java/com/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algafood/domain/model/Restaurante.java
@@ -33,5 +33,8 @@ public class Restaurante {
     inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
     private List<FormaPagamento> formasPagamentos = new ArrayList<>();
 
+    @Embedded
+    private Endereco endereco;
+
 
 }
